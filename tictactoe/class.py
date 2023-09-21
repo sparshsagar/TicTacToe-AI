ec632296d1c8a83457e1cbc0e51c4888d72f914b
Sparsh Sagar <64336268+sparshsagar@users.noreply.github.com>
2023-09-21 18:36:19 -0400

    Add files via upload

---
+++ tictactoe/class.py
@@ -0,0 +1,10 @@
+Begin
+Declare user,admin, student
+Print " admin = 1 \n student = 2 \n enter 1 or 2 for your user "
+Read user
+if user == 1
+    print "admin function"
+elif user == 2
+    print "student function"
+else
+    print "invalid input"
---
+++ tictactoe/pyp.py
@@ -0,0 +1,9 @@
+Begin
+ Declare scr1,src2,src3,src4,src5
+ Display " enter marks in 5 subjects:"
+ Read src1,src2,src3,src4,src5
+ sum = src1+src2+src3+src4+src5
+ avg = sum/5
+ Display " total marks of 5 subjects :", sum
+ Display " average of 5 subject marks :", avg
+ END
